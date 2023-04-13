80c7f94f436846670ec7ba9aaba4680be19058a3
Oleksandr <lysakova@yahoo.com>
2023-04-13 20:37:59 +0200

    Add tech for projects

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,27 +1,6 @@
 import Image from 'next/image';
 import { FaGithub } from 'react-icons/fa';
 import { IoMdOpen } from 'react-icons/io';
-import project from '../../../public/assets/projects/time_management/time_web_screen.png';
-
-// {
-//     title: 'Employee Time Management System',
-//     about: `This is a commercial project that was developed based on client needs and is now
-//        being used by a warehouse company to manage employee work times, vacations, and attendance
-//        in various work environments. The system includes a mobile app for employees and an admin web application for managers.`,
-//     tech: [
-//       'React',
-//       'React Native',
-//       'MongoDB Realm',
-//       'MongoDB Serverless Functions',
-//       'Realm Web',
-//       'Material UI',
-//     ],
-//     links: {
-//       live: 'https://best-ever-magazyn.netlify.app/',
-//       github: 'https://github.com/pologora/employee_time_management',
-//     },
-//     screenShoots: [home, employees, vacations, vacations2, raports, raports2],
-//   },
 
 function Project({
   title, about, tech, links, screenShoots,
@@ -29,24 +8,30 @@ function Project({
   return (
     <article>
       <div className="mb-10">
-        <div className="flex justify-between place-items-center gap-10 mb-10 flex-col md:flex-row">
+        <div className="flex justify-between md:place-items-center place-items-end gap-10 mb-10 flex-col md:flex-row">
           <h2>{title}</h2>
           <div className="flex gap-10 md:gap-6">
-            <FaGithub size={25} />
-            <IoMdOpen size={25} />
+            <a href={links.github} target="blank">
+              <FaGithub size={25} />
+            </a>
+            <a href={links.live} target="blank">
+              <IoMdOpen size={25} />
+            </a>
           </div>
         </div>
-        <p>
-          This is a commercial project that was developed based on client needs and is now being
-          used by a warehouse company to manage employee work times, vacations, and attendance in
-          various work environments. The system includes a mobile app for employees and an admin web
-          application for managers.
-        </p>
-        <ul />
+        <p className="mb-5">{about}</p>
+        <h3 className="text-center mb-5">Tech:</h3>
+        <ul className="flex flex-wrap gap-4">
+          {tech.map((item) => (
+            <li className="text-projects font-extralight" key={item}>
+              {item}
+            </li>
+          ))}
+        </ul>
       </div>
       <div className="projects relative z-[-10]">
         <div className="">
-          <Image src={project} />
+          <Image src={screenShoots[0]} />
         </div>
       </div>
     </article>
