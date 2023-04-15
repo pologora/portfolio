3707edf54f140d99088017ca4cad5472cb89b0d4
Oleksandr <lysakova@yahoo.com>
2023-04-15 16:21:39 +0200

    Change contact styles

---
+++ src/components/Contact/ContactVariation.jsx
@@ -0,0 +1,26 @@
+function ContactVariation({
+  text, title, color, link,
+}) {
+  const linkClassName = title === 'Resume' ? 'font-bold' : `text-${color}`;
+
+  return (
+    <li className="sm:ml-10 flex place-items-center gap-2">
+      <div className="w-3 h-3 bg-contact" />
+      <div className="flex place-items-baseline gap-2">
+        <p>{text}</p>
+        {' '}
+        <a
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+          download={title === 'Resume' ? 'resume.pdf' : undefined}
+          className={`${linkClassName} font-bold`}
+        >
+          {title}
+        </a>
+        {' '}
+      </div>
+    </li>
+  );
+}
+export default ContactVariation;
---
+++ src/data/socials.js
@@ -0,0 +1,25 @@
+export default [
+  {
+    text: 'Send me an email at',
+    title: 'Gmail',
+    color: 'home',
+    link: 'mailto:lysakov555@gmail.com',
+  },
+  {
+    text: 'Check out my work on',
+    title: 'GitHub',
+    color: 'about',
+    link: 'https://github.com/pologora',
+  },
+  {
+    text: 'Connect with me on',
+    title: ' LinkedIn',
+    color: 'projects',
+    link: 'https://www.linkedin.com/in/oleksandr-lysakov/',
+  },
+  {
+    text: 'You can also download',
+    title: 'Resume',
+    link: '/resume.pdf',
+  },
+];
