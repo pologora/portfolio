6507331d622fb22274c1c539ececd433f5b1f02b
Oleksandr <lysakova@yahoo.com>
2023-07-11 06:04:45 +0200

    Change text in bio as graduate

--- src/data/bioText.js
+++ src/data/bioText.js
@@ -1,36 +1,40 @@
 export const full = [
-  `Greetings, friends! Allow me to introduce myself - I am a student at my last year of
-        computer science studies, with a passion for creating visually appealing and user-friendly
-        websites. Before embarking on my career in tech, I earned a Bachelor's degree in
-        management. However, my true calling has always been in web development. As a hardworking
-        and motivated individual, I am constantly striving to hone my skills and stay up-to-date
-        with the latest technologies.`,
-  `When I'm not slinging code, you can find me flexing my brain muscles with a game of
-        chess, exploring new worlds through my favorite science fiction books, or getting lost in a
-        great film. Playing chess helps me keep my problem-solving and strategic thinking sharp,
-        while reading allows me to improve my imagination and communication skills. And movies?
-        They're not just entertainment, but an inspiring source of storytelling and visual
-        design.`,
-  `Now as a student at my last year, I am actively seeking a Junior Web Developer position in a
-        company where I can put my skills and passion to good use and grow my skills. If you are
-        looking for a Junior web developer or know of any available positions, I would love to hear
-        from you.`,
+  `Greetings! I am a recent Computer Science graduate with a passion for crafting visually appealing and
+   user-friendly websites. Prior to my foray into the tech industry, I earned a Bachelor's degree in Management.
+    Despite this, my true passion has always lied within the realm of web development. As a motivated and diligent 
+    individual, I continually strive to refine my skills and keep up-to-date with the latest technologies.`,
+
+  `In my spare time, you can find me keeping my problem-solving and strategic thinking skills sharp with a game 
+  of chess, venturing into new universes through my favorite science fiction books, or immersing myself in a 
+  captivating film. These activities are not just hobbies for me - chess enhances my strategic thinking, reading
+   fosters my imagination and communication skills, and movies offer a rich source of storytelling and visual design 
+   inspiration.`,
+
+  `As a new graduate, I am eagerly seeking a Junior Web Developer position in a company where I can apply my
+   skills and passion for web development, and continue to grow professionally. If you have any available positions 
+   or know of any that may be a good fit, I would love to hear from you.`,
 ];
 
 export const medium = [
-  `Hello there! As a motivated computer science student in my last year, I specialize in designing eye-catching
-     and user-friendly websites. I'm on the hunt for a Junior Web Developer opportunity to apply my knowledge
-      and further develop my skills in a professional context.`,
-  `When I'm not coding, I enjoy stimulating my 
-      mind with chess, immersing myself in the imaginative worlds of science fiction literature, and gaining 
-      inspiration from the storytelling and visual aesthetics in movies.`,
-  `  I'm eager to join a forward-thinking
-       company where I can contribute to innovative projects and grow as a web developer.`,
+  `Hello there! As a motivated recent graduate in Computer Science, 
+  I specialize in designing eye-catching and user-friendly websites. 
+  I am on the lookout for a Junior Web Developer position where I can 
+  apply my knowledge, passion, and skills in a professional context.`,
+
+  `When I'm not immersing myself in code, you can find me stimulating
+       my mind with a game of chess, exploring the imaginative universes
+       found in science fiction literature, and drawing inspiration from 
+       the compelling storytelling and visual aesthetics in movies.`,
+
+  `Now as a proud graduate, I'm eager to join a forward-thinking company
+       where I can contribute to innovative projects, learn from industry 
+       professionals, and grow as a web developer. If you know of any opportunities
+        or have any positions available, I'm all ears and would love to hear from you.`,
 ];
 
 export const small = [
-  ` Hi! I'm a computer science student,
-   specializing in creating visually engaging and user-friendly websites.
-    Looking for a Junior Web Developer role to grow my skills. Chess enthusiast,
-     sci-fi reader, and film lover!`,
+  `Hi! I'm a recent Computer Science graduate, specializing in creating visually engaging and user-friendly websites.
+   Now on the hunt for a Junior Web Developer role to apply my skills and further my growth in the field. Off the clock,
+    I'm a chess enthusiast, 
+  sci-fi reader, and film lover, always eager to stimulate my mind and draw inspiration from various sources!`,
 ];
