dd6329cf285fbe6003a3d57286f2684b8c8d2e5d
Oleksandr <lysakova@yahoo.com>
2023-11-27 20:20:55 +0100

    Bug fix: add optional chaining

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -12,17 +12,17 @@ function Project({
         <div className="flex justify-between md:place-items-center place-items-center gap-10 mb-10 flex-col md:flex-row">
           <h3 className="uppercase font-semibold">{title}</h3>
           <div className="flex gap-10 md:gap-6 text-projects">
-            <a href={links.github} target="blank">
+            <a href={links?.github} target="blank">
               <FaGithub size={25} />
             </a>
-            <a href={links.live} target="blank">
+            <a href={links?.live} target="blank">
               <IoMdOpen size={25} />
             </a>
           </div>
         </div>
         <p className="mb-5">{about}</p>
       </div>
-      <div className="">
+      <div>
         <div>
           <ScreenShotsList screenShots={screenShots} />
         </div>
