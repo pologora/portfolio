b8bf031566bf7c24df7e763cadfc9bfde80b56c0
Oleksandr <lysakova@yahoo.com>
2024-04-30 15:06:16 +0200

    feat: update tech stack

--- src/data/techList.js
+++ src/data/techList.js
@@ -1,14 +1,5 @@
 export default {
-  'Front-end': [
-    'HTML',
-    'CSS',
-    'JavaScript',
-    'TypeScript',
-    'React',
-    'Next.js',
-    'Material UI',
-    'Tailwindcss',
-  ],
-  'Back-end': ['MongoDB', 'REST API', 'Python', 'Flask RESTful'],
+  'Front-end': ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'React', 'Next.js', 'Material UI', 'Tailwindcss'],
+  'Back-end': ['Node.js', 'MongoDB', 'SQL', 'REST API'],
   Mobile: ['React Native'],
 };
