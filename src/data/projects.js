9b187d980511eb4101fe7f229572bfce244cab2b
Oleksandr <lysakova@yahoo.com>
2024-12-02 08:28:35 +0100

    feat: add alias game project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,45 @@
 export default [
   {
     en: {
-      title: 'Employee Time Management System',
+      title: 'Alias Multiplayer Game (2024)',
+      about:
+        'Alias is a real-time multiplayer word game developed as a group project, led by me, during an internship at Solvd, Inc. The game leverages modern web technologies to provide seamless real-time gameplay, chat, and word validation functionality.',
+      tech: {
+        'Front-end': ['Handlebars'],
+        'Back-end': ['Node.js', 'MongoDB', 'Express', 'TypeScript'],
+      },
+      links: {
+        live: 'https://alias.webdevolek.stronawcal.pl',
+        github: 'https://github.com/pologora/alias',
+      },
+      screenShots: {
+        path: '/assets/projects/alias/',
+        web: ['login.png', 'home.png', 'gameLobby.png', 'game.png', 'profile.png', 'history.png'],
+        mobile: ['login.png', 'home.png', 'gameLobby.png', 'game.png', 'profile.png', 'history.png'],
+      },
+    },
+    pl: {
+      title: 'Alias Gra Wieloosobowa (2024)',
+      about:
+        'Alias to gra słowna w czasie rzeczywistym, opracowana jako projekt grupowy, którym kierowałem, podczas stażu w firmie Solvd, Inc. Gra wykorzystuje nowoczesne technologie webowe, aby zapewnić płynną rozgrywkę w czasie rzeczywistym, czat i funkcję weryfikacji słów.',
+      tech: {
+        'Front-end': ['Handlebars'],
+        'Back-end': ['Node.js', 'MongoDB', 'Express', 'TypeScript'],
+      },
+      links: {
+        live: 'https://alias.webdevolek.stronawcal.pl',
+        github: 'https://github.com/pologora/alias',
+      },
+      screenShots: {
+        path: '/assets/projects/alias/',
+        web: ['login.png', 'home.png', 'gameLobby.png', 'game.png', 'profile.png', 'history.png'],
+        mobile: ['login.png', 'home.png', 'gameLobby.png', 'game.png', 'profile.png', 'history.png'],
+      },
+    },
+  },
+  {
+    en: {
+      title: 'Employee Time Management System (2023)',
       about:
         'This is a commercial, freelance full stack project that was developed based on client needs and is now being used by a warehouse company to manage employee work schedules, track vacations, and generate various reports. The system includes a mobile and web app for employees, an admin web application for managers.',
       tech: {
@@ -27,7 +65,7 @@ export default [
       },
     },
     pl: {
-      title: 'System ewidencji pracy',
+      title: 'System ewidencji pracy (2023)',
       about:
         'To jest komercyjny projekt full stack, który został opracowany na podstawie potrzeb klienta i jest obecnie używany przez firmę magazynową do zarządzania grafikami pracy pracowników, śledzenia urlopów i generowania różnych raportów. System obejmuje aplikację mobilną dla pracowników i aplikację internetową dla administratorów.',
       tech: {
@@ -55,7 +93,7 @@ export default [
   },
   {
     en: {
-      title: 'Audiophile e-commerce website',
+      title: 'Audiophile e-commerce website (2023)',
       about:
         'E-commerce website I developed to demonstrate my skills as a frontend developer. This multi-page full responsive e-commerce site, built with Next.js, TypeScript, and Tailwind CSS, showcases my proficiency in these modern web development technologies. I meticulously translated a Figma design file into a fully functional and responsive website.',
       tech: {
@@ -72,7 +110,7 @@ export default [
       },
     },
     pl: {
-      title: 'Strona e-commerce Audiophile',
+      title: 'Strona e-commerce Audiophile (2023)',
       about:
         'Strona e-commerce, którą stworzyłem, aby pokazać moje umiejętności jako front-end developera. Ta wielostronicowa, w pełni responsywna witryna e-commerce, zbudowana przy użyciu Next.js, TypeScript i Tailwind CSS, prezentuje moją biegłość w tych nowoczesnych technologiach webowych. Skrupulatnie przetłumaczyłem plik projektu Figma na w pełni funkcjonalną i responsywną witrynę internetową.',
       tech: {
@@ -92,7 +130,7 @@ export default [
 
   {
     en: {
-      title: 'NFT project on Cronos blockchain',
+      title: 'NFT project on Cronos blockchain (2022)',
       about:
         'The "CPunks" NFT Collection is an innovative project on the Cronos blockchain, consisting of 10,000 uniquely generated characters. With meticulous design and attention to detail, no two characters are identical. The collection features 9 alien species, 24 intelligent apes, and 88 zombies, all possessing 87 unique attributes, including rare traits, accessories, and special abilities.',
       tech: {
@@ -110,7 +148,7 @@ export default [
       },
     },
     pl: {
-      title: 'Projekt NFT na blockchainie Cronos',
+      title: 'Projekt NFT na blockchainie Cronos (2022)',
       about:
         'Kolekcja NFT "CPunks" to innowacyjny projekt oparty na blockchainie Cronos, składający się z 10 000 unikalnie generowanych postaci. Dzięki szczegółowemu projektowi i dbałości o detale, żadne dwie postacie nie są identyczne. Kolekcja obejmuje 9 gatunków obcych, 24 inteligentne małpy i 88 zombiaki, wszystkie posiadające 87 unikalnych cech, w tym rzadkie cechy, akcesoria i specjalne zdolności.',
       tech: {
